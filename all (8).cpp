69da7c73584895bd30f071c0f969e0fb9e0ec5b5
Blueeyeed <Python-today5@yandex.ru>
2026-02-06 09:09:06 +0300

    Add files via upload

---
+++ all (8).cpp
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="utf-8">
+	<title>Размеры шрифтов</title>
+
+	<style type="text/css">
+		
+		body {
+		  text-align: center;
+		  margin: 10;
+		  font-family: Verdana, Arial, sans-serif;
+		  font-size: 20px;
+		}
+	</style>
+
+</head>
+<body>
+	<!-- текст с основным размером шрифта -->
+	<p>Привет, это журнал «Код»!</p>
+	<!-- эта строка будет иметь шрифт в 2 раза больше -->
+	<p style="font-size: 2em;">А это — статья про размеры шрифтов</p>
+	<!-- а эта — в 0,7 раза меньше, чем основной шрифт-->
+	<p style="font-size: 0.7em;">И здесь всё постоянно меняется</p>
+
+</body>
+</html>
